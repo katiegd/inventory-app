d69744ef3bbf72130b7cfd07d3057af0b085cd3c
Katie Duryea <155897343+katiegd@users.noreply.github.com>
2024-10-16 18:56:41 -0400

    Product sorting and filtering complete. Next categories and add/delete products/categories.

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,8 +1,35 @@
 const db = require("../db/queries");
 
 async function getAllProducts(req, res) {
-  const products = await db.showAllProducts();
-  res.render("products", { title: "Products", products });
+  const sort = req.query.sort;
+  const selectedCategory = req.query.category;
+  const selectedColors = req.query.color;
+
+  const listedCategories = await db.getUniqueCategories();
+  const listedColors = await db.getUniqueColors();
+
+  let products;
+
+  if (sort === "priceAsc" || sort === "priceDesc") {
+    products = await db.sortByPrice({ sort });
+  } else if (sort === "nameAsc" || sort === "nameDesc") {
+    products = await db.sortByName({ sort });
+  } else if (selectedCategory) {
+    products = await db.filterByCategory({ selectedCategory });
+  } else if (selectedColors) {
+    products = await db.filterByColor({ selectedColors });
+  } else {
+    products = await db.showAllProducts();
+  }
+  res.render("products", {
+    title: "Products",
+    products,
+    sort,
+    selectedCategory,
+    listedCategories,
+    selectedColors,
+    listedColors,
+  });
 }
 
 module.exports = {
--- db/queries.js
+++ db/queries.js
@@ -2,10 +2,83 @@ const pool = require("./pool");
 
 async function showAllProducts() {
   const { rows } = await pool.query("SELECT * FROM inventory;");
-  console.log(rows);
+  return rows;
+}
+
+async function sortByPrice({ sort = "priceASC" }) {
+  const order = sort.toUpperCase() === "PRICEDESC" ? "DESC" : "ASC";
+
+  const { rows } = await pool.query(
+    `SELECT * FROM inventory ORDER BY price ${order}`
+  );
+  return rows;
+}
+
+async function sortByName({ sort = "nameASC" }) {
+  const order = sort.toUpperCase() === "NAMEDESC" ? "DESC" : "ASC";
+
+  const { rows } = await pool.query(
+    `SELECT * FROM inventory ORDER BY name ${order}`
+  );
+  return rows;
+}
+
+async function getUniqueCategories() {
+  const { rows } = await pool.query(
+    "SELECT DISTINCT category FROM inventory ORDER BY category ASC"
+  );
+  const categories = rows.map((row) => row.category);
+  return categories;
+}
+
+async function filterByCategory({ selectedCategory }) {
+  // Make sure selectedCategory is an array to use with .map()
+  if (!Array.isArray(selectedCategory)) {
+    selectedCategory = [selectedCategory];
+  }
+
+  // Make placeholder for each selection "COLOR, "
+  const placeholder = selectedCategory
+    .map((_, index) => `$${index + 1}`)
+    .join(", ");
+  const query = `SELECT * FROM inventory WHERE category IN (${placeholder})
+  ORDER BY category ASC`;
+
+  // Pass the query as well as the category for the placeholder function to work
+  const { rows } = await pool.query(query, selectedCategory);
+  return rows;
+}
+
+async function getUniqueColors() {
+  const { rows } = await pool.query(
+    "SELECT DISTINCT color FROM inventory ORDER BY color ASC"
+  );
+  const colors = rows.map((row) => row.color);
+  return colors;
+}
+
+async function filterByColor({ selectedColors }) {
+  if (!Array.isArray(selectedColors)) {
+    selectedColors = [selectedColors];
+  }
+
+  const placeholder = selectedColors
+    .map((_, index) => `$${index + 1}`)
+    .join(", ");
+
+  const query = `SELECT * FROM inventory WHERE color IN (${placeholder})
+  ORDER BY color ASC`;
+
+  const { rows } = await pool.query(query, selectedColors);
   return rows;
 }
 
 module.exports = {
   showAllProducts,
+  sortByPrice,
+  sortByName,
+  filterByCategory,
+  getUniqueColors,
+  getUniqueCategories,
+  filterByColor,
 };
